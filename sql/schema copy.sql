125d50d55f7eecd4b428f1d7357e2b4809fc9939
Alex Madrid <alexmadrid326@gmail.com>
2026-02-21 12:42:13 -0400

    espacio en el filtro

--- sql/schema copy.sql
+++ sql/schema copy.sql
@@ -137,8 +137,6 @@ CREATE TABLE IF NOT EXISTS business_clients (
   UNIQUE KEY (business_id)
 );
 
---------------------actualizaciones a las tablas de la base de datos
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- índices 
 CREATE INDEX IF NOT EXISTS idx_categories_name ON categories(name);
@@ -149,9 +147,6 @@ CREATE INDEX  idx_items_user_id ON items(user_id);
 CREATE INDEX idx_movements_user_id ON movements(user_id);
 CREATE INDEX idx_users_role ON users(role); 
 
--- Agregar campo cedula a la tabla users para identificación única
-ALTER TABLE users 
-ADD COLUMN cedula VARCHAR(20) UNIQUE AFTER username;
 
 -- Agregar campo client_id a la tabla users para relacionar con business_clients
 ALTER TABLE users ADD COLUMN client_id INT DEFAULT NULL AFTER role;
@@ -208,34 +203,4 @@ INSERT IGNORE INTO categories (name, description) VALUES
 insert ignore into suppliers (name, contact, phone, email, address) values
 ('Indatech', 'Jose Perez', '04242977384', 'Info@indatechca.com', 'c.c.c.t, Torre C, piso 7 oficinas 707 C. Ernesto Blohm, Caracas 1060, Distrito Capital 
 Venezuela');
-
-
---insertar equipos del iuti
-insert ignore into items (id, user_id, sku, name, description, quantity, category_id, supplier_id) 
-values
-(1,	1,	'IUTI-DIRNAC-002',	'equipo 01', 	'Interl Core I3-2120 3.30 ghz	8GB	500GB SSD	Windows 10',	1,	1,	1),
-(2,	1,	'IUTI-SUBDIR-001',	'equipo 02',	'AMD Athlon II X3 435	8GB	500GB HDD	Windows 10',	1,	1,	1),
-(3,	1,	'IUTI-ASTDNC-002',	'equipo 03',	'Intel Core I5 8500 3GHZ	8GB	500 SSD	Windows 11',	1,	1,	1),
-(4,	1,	'IUTI-CACADEMICA',	'equipo 04',	'Intel Core I5 4590S 3GHZ	8GB	256 SSD	Windows 7 Ultimate',	1,	1,	1),
-(5,	1,	'IUTI-ASTCA-001',	'equipo 05',	'Intel Core I5 3470S 2,9 GHZ	6GB	500 HDD	Windows 10',	1,	1,	1),
-(6,	1,	'IUTI-ACDM-001',	'equipo 06',	'Intel Core I5 8500 3GHZ	8GB	256 HDD	Windows 11',	1,	1,	1),
-(7,	1,	'IUTI-ADMON-001',	'equipo 07',	'Intel Core I5 6500 3,20 GHZ	8GB	256 SSD	Windows 10',	1,	1,	1),
-(8, 1,	'IUTI-ASTADM-001',	'equipo 08',	'Intel(R) Core(TM) i5-6500 CPU @ 3.20GHz   3.19 GHz	8GB	256 SSD	Windows 11',	1,	1,	1),
-(9, 1,	'IUTI-ASTCONTBL',	'equipo 09',	'Pentium(R) Dual Core CPU E5500 2.8 GHz	4GB	128 HHD	Windows 7 Ultimate',	1,	1,	1),
-(10, 1,	'IUTI-CAJA01',	'equipo 10',	'Intel(R) Core(TM) i3-2100 CPU @ 3.10GHz   3.10 GHz	4GB	500 SSD	Windows 10',	1,	1,	1),
-(11, 1,	'IUTI-CAJA02',	'equipo 11',	'Intel(R) Core(TM) i3-4130 CPU @ 3.10GHz   3.40 GHz	8GB	256 SSD	Windows 10',	1,	1,	1),
-(12, 1,	'IUTI-CLIPER-001',	'equipo 12',	'Intel(R) Core(TM) i7-7700 CPU @ 3.60GHz	8GB	256 SSD	Windows 7 Ultimate',	1,	1,	1),
-(13,	1,	'IUTI-ARCHV-001',	'equipo 13',	'Intel(R) Core(TM) i7-7700 CPU @ 3.60GHz	8GB	256 ssd	Windows 7 Ultimate',	1, 1,	1),
-(14,	1,	'TAQ001',	'equipo 14',	'Intel(R) Core(TM) i3-2130 CPU @ 3.40GHz	5GB	256 ssd	Windows 7 Ultimate',	1,	1, 1),
-(15,	1,	'TAQ002',	'equipo 15',	'Intel(R) Core(TM) i3-4130 CPU @ 3.40GHz	8GB	256 ssd Windows 7 Ultimate',	1,	1,	1),
-(16,	1,	'IUTI-ASICE-001',	'equipo 16',	'Intel(R) Core(TM) i3-4130 CPU @ 3.40GHz	8GB	500GB HDD	Windows 7 Ultimate',	1,	1,	1),
-(17,	1,	'IUTI-CORDCE-001',	'equipo 17',	'Intel(R) Core(TM) i5-4570 CPU @ 3.20 GHz	8GB	500GB HDD	Windows 7 Ultimate',	1,	1,	1),
-(18,	1,	'IUTI-CBASIC-001',	'equipo 18',	'Intel(R) Core(TM) i7-7700 CPU @ 3.60GHz   3.60 GHz	8GB	256SSD	Windows 11',	1,	1,	1),
-(19,	1,	'IUTI-RINST-002',	'equipo 19',	'Intel(R) Core(TM) i3-4130 CPU @ 3.40GHz   3.40 GHz	8GB	256SSD	Windows 11',	1,	1,	1),
-(20,	1,	'IUTI-RINST-001',	'equipo 20',	'Intel(R) Core(TM) i5-8500 CPU @ 3.00GHz (3.00 GHz)	16GB	500 SSD	Windows 10',	1,	1,	1),
-(21,	1,	'IUTI-BIBLIO-001',	'equipo 21',	'Intel(R) Core(TM) i5-6500 CPU @ 3.20GHz (3.20 GHz)	8gb	256 SSD	Windows 10',	1,	1,	1),
-(22,	1,	'IUTI-CPAS-001',	'equipo 22',	'Intel(R) Core(TM) i3-4130 CPU @ 3.40GHz   3.40 GHz	8gb	256 SSD	Windows 10',	1,	1,	1),
-(23,	1, 'IUTI-CEXT-001',	'equipo 23',	'Intel(R) Core(TM) i5-6500 CPU @ 3.20GHz (3.20 GHz)	8gb	256 SSD	Windows 10',	1,	1,	1);
-
-
 
